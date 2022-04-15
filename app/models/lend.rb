b014e0bbb4355ab0ea8d422b3aefb2dae9ab27ab
Dayane <dls6@cin.ufpe.br>
2022-04-15 17:59:16 -0300

    fix indentation

--- app/models/lend.rb
+++ app/models/lend.rb
@@ -17,8 +17,8 @@ class Lend < ApplicationRecord
 
   def user_is_valid?
     unless user_id.nil?
-    return if Lend.find_by(id: id)
-     lends_count = Lend.where(user_id: user_id).count 
+      return if Lend.find_by(id: id)
+      lends_count = Lend.where(user_id: user_id).count 
       if lends_count >= 2
         errors.add(:user_id, "can not borrow a book right now")
       end
